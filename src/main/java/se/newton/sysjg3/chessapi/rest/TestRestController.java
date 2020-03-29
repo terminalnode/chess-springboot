1af3efaaa9a3ca6a9875d6e5141e28451ec6dc60
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-03-29 22:50:19 +0200

    testing datasource url

--- src/main/java/se/newton/sysjg3/chessapi/rest/TestRestController.java
+++ src/main/java/se/newton/sysjg3/chessapi/rest/TestRestController.java
@@ -1,5 +1,6 @@
 package se.newton.sysjg3.chessapi.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class TestRestController {
+  @Value("${spring.datasource.url}")
+  private String dbUrl;
 
   @GetMapping("/eclipse")
   public String getEclipse() {
-    return "Friends don't let friends get Eclipse.";
+    return "Friends don't let friends get Eclipse. " + dbUrl;
   }
 }

0aba20f72150fba2d21423b522ab19d0f6c7b9e4
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-03-30 19:15:00 +0200

    validate data before creating user

--- src/main/java/se/newton/sysjg3/chessapi/rest/PlayerRestController.java
+++ src/main/java/se/newton/sysjg3/chessapi/rest/PlayerRestController.java
@@ -3,6 +3,8 @@ package se.newton.sysjg3.chessapi.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import se.newton.sysjg3.chessapi.entity.Player;
+import se.newton.sysjg3.chessapi.rest.exceptions.PlayerCreateMissingFieldsException;
+import se.newton.sysjg3.chessapi.rest.exceptions.PlayerCreateUsernameTaken;
 import se.newton.sysjg3.chessapi.service.PlayerService;
 
 import java.util.List;
@@ -24,7 +26,21 @@ public class PlayerRestController {
 
   @PostMapping("/players")
   public Player createUser(@RequestBody Player player) {
+    // Setting ID to 0 is basically unsetting it
     player.setId(0);
+
+    // Validate input before creating user
+    if (player.getName() == null && player.getPassword() == null) {
+      throw new PlayerCreateMissingFieldsException("Missing fields: username, password");
+    } else if (player.getName() == null) {
+      throw new PlayerCreateMissingFieldsException("Missing field: username");
+    } else if (player.getPassword() == null) {
+      throw new PlayerCreateMissingFieldsException("Missing field: password");
+    } else if (!playerService.checkUsernameAvailability(player.getName())) {
+      throw new PlayerCreateUsernameTaken(String.format("Username taken: %s", player.getName()));
+    }
+
+    // Create and return the new player
     playerService.create(player);
     return player;
   }

9f3c9dbea8395bf80161501b3666ceb2153f3910
SebastianLindfors <sebastianlindfors@gmail.com>
2020-03-31 16:09:19 +0200

    Added TokenDAO and TokenDAOHibernate

--- src/main/java/se/newton/sysjg3/chessapi/dao/TokenDAO.java
+++ src/main/java/se/newton/sysjg3/chessapi/dao/TokenDAO.java
@@ -1,4 +1,9 @@
 package se.newton.sysjg3.chessapi.dao;
 
+import se.newton.sysjg3.chessapi.entity.Player;
+import se.newton.sysjg3.chessapi.entity.Token;
+
 public interface TokenDAO {
+
+    public Token createTokenForPlayer(Player player);
 }
--- src/main/java/se/newton/sysjg3/chessapi/dao/TokenDAOHibernate.java
+++ src/main/java/se/newton/sysjg3/chessapi/dao/TokenDAOHibernate.java
@@ -1,4 +1,31 @@
 package se.newton.sysjg3.chessapi.dao;
 
-public class TokenDAOHibernate {
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import se.newton.sysjg3.chessapi.entity.Player;
+import se.newton.sysjg3.chessapi.entity.Token;
+
+import javax.persistence.EntityManager;
+
+public class TokenDAOHibernate implements TokenDAO {
+    private EntityManager entityManager;
+
+    @Autowired
+    public TokenDAOHibernate(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public Token createTokenForPlayer(Player player) {
+        Session session = entityManager.unwrap(Session.class);
+
+        Token token = new Token(player);
+        player.addToken(token);
+
+        session.save(token);
+
+        return token;
+    }
+
+
+
 }

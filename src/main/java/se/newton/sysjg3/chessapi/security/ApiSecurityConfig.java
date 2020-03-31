a5d7060d5dd648d055c8713202b48463f9a66956
SebastianLindfors <sebastianlindfors@gmail.com>
2020-03-31 11:52:16 +0200

    Enabled spring security and enabled post requests to /api/palyers without authentication

---
+++ src/main/java/se/newton/sysjg3/chessapi/security/ApiSecurityConfig.java
@@ -0,0 +1,31 @@
+package se.newton.sysjg3.chessapi.security;
+
+
+
+import org.springframework.context.annotation.Configuration;
+
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ApiSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated();
+
+
+    }
+
+    public void configure(WebSecurity web)
+            throws Exception {
+        web.ignoring().antMatchers(HttpMethod.POST, "/api/players");
+    }
+}

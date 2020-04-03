399997fcbb0514f5ece78c5a94e921e9b7d9d7f1
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-04-04 01:44:58 +0200

    make POST /game/{gameId} return the made move

--- src/main/java/se/newton/sysjg3/chessapi/rest/controllers/GameRestController.java
+++ src/main/java/se/newton/sysjg3/chessapi/rest/controllers/GameRestController.java
@@ -1,12 +1,9 @@
 package se.newton.sysjg3.chessapi.rest.controllers;
 
-import com.fasterxml.jackson.databind.exc.InvalidDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import se.newton.sysjg3.chessapi.entity.Challenge;
 import se.newton.sysjg3.chessapi.entity.Game;
 import se.newton.sysjg3.chessapi.helpers.ChessMove;
-import se.newton.sysjg3.chessapi.service.ChallengeService;
 import se.newton.sysjg3.chessapi.service.GameService;
 
 import java.util.List;
@@ -23,35 +20,26 @@ public class GameRestController {
   }
 
   //----- GET Mappings -----//
-
   @GetMapping("/games")
-  public List<Game> getAllCurrentPlayerGamesFromToken(@RequestHeader(value="Token") String tokenString) {
+  public List<Game> getAllCurrentPlayerGamesFromToken(
+      @RequestHeader(value="Token") String tokenString) {
     return gameService.getCurrentPlayerGamesFromToken(tokenString);
   }
 
   @GetMapping("/games/{gameId}")
-  public Game getCurrentPlayerGamesFromGameId(@RequestHeader(value="Token") String tokenString,
-                                                     @PathVariable long gameId) throws RuntimeException {
-      
-      return gameService.getCurrentPlayerGameFromGameId(tokenString, gameId);
-
+  public Game getCurrentPlayerGamesFromGameId(
+      @RequestHeader(value="Token") String tokenString,
+      @PathVariable long gameId) {
+    return gameService.getCurrentPlayerGameFromGameId(tokenString, gameId);
   }
 
-  //----- Post Mappings -----//
-
+  //----- POST Mappings -----//
   @PostMapping("/games/{gameId}")
-  public String makeMoveInGame(@RequestHeader(value="Token") String tokenString,
-                               @RequestBody ChessMove move,
-                               @PathVariable long gameId) {
-    try {
-      gameService.makeMove(move, gameId, tokenString);
-    }
-    catch (Exception e) {
-      System.out.println(">>>CAUGHT THE EXCEPTION THIS TIME");
-    }
-    return "Move made succesfully";
+  public ChessMove makeMoveInGame(
+      @RequestHeader(value="Token") String tokenString,
+      @RequestBody ChessMove move,
+      @PathVariable long gameId) {
+    gameService.makeMove(move, gameId, tokenString);
+    return move;
   }
-
-
-
 }
